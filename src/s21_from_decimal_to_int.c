4847d9039cc9259d904fb48ac2840e2bd3ca766d
Oleg <bsm2003@list.ru>
2024-07-05 22:54:12 +0300

    fix

--- src/s21_from_decimal_to_int.c
+++ src/s21_from_decimal_to_int.c
@@ -6,11 +6,11 @@
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
     int result_code = 0;
 
-    if (dst == s21_NULL || check_int_range(src)) {
+    if (dst == s21_NULL || check_decimal(src) || check_int_range(src)) {
         result_code = 1;
     } else {
         s21_decimal truncated_decimal = get_new_decimal();
-        // TODO: s21_truncate(src, &truncated_decimal);
+        s21_truncate(src, &truncated_decimal);
 
         *dst = 0;
         for (int i = 0; i < 32; i++) {
