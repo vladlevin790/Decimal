e6f4a700ae9c143aac60e56f58d9bba81b9fbf80
Johnna Evver <johnnaev@mu-e4.yar.21-school.ru>
2024-07-22 17:24:00 +0300

    tests

--- src/other_func/s21_other_func.c
+++ src/other_func/s21_other_func.c
@@ -15,39 +15,45 @@ int s21_negate(s21_decimal value, s21_decimal *result) {
 }
 
 int s21_truncate(s21_decimal value, s21_decimal *result) {
-    int result_code = 0;
+  int result_code = 0;
 
-    if (result == s21_NULL || check_decimal(value)) {
-        result_code = 1;
-    } else {
-        clear_decimal(result);
-
-        int current_exponent = get_decimal_exponent(value);
-        set_decimal_exponent(&value, 0);
-
-        s21_decimal decimal_ten_div = get_new_decimal();
-        s21_from_int_to_decimal(10, &decimal_ten_div);
-
-        while (current_exponent > 0) {
-            s21_div(value, decimal_ten_div, &value);
-
-            current_exponent--;
-        }
-
-        *result = value;
+  if (result == s21_NULL || check_decimal(value)) {
+    result_code = 1;
+  } else {
+    clear_decimal(result);
+    int exponent = get_decimal_exponent(value);
+    int sign = get_decimal_sign(value);
+    set_decimal_exponent(&value, 0);
+    set_decimal_sign(&value, 0);
+    s21_big_decimal div_1 = {
+        {get_decimal_with_int_value(10), get_new_decimal()}};
+    s21_big_decimal big_value = {{value, get_new_decimal()}};
+    s21_big_decimal rev_value = {{get_new_decimal(), get_new_decimal()}};
+    s21_big_decimal big_result = big_value;
+    while (exponent != 0) {
+      s21_big_div(big_result, div_1, &big_result, &rev_value);
+      exponent--;
     }
+    *result = big_result.decimal[0];
+    set_decimal_sign(result, sign);
+    set_decimal_exponent(result, 0);
+  }
 
-    return result_code;
+  return result_code;
 }
 
 int s21_round(s21_decimal value, s21_decimal *result) {
   int result_code = 0;
   if (result == s21_NULL || check_decimal(value)) {
     result_code = 1;
+  } else if (!get_decimal_sign(value)) {
+    clear_decimal(result);
+    s21_decimal tmp = {{5, 0, 0, 65536}};
+    s21_add(value, tmp, result);
+    s21_truncate(*result, result);
   } else {
     clear_decimal(result);
-    s21_decimal tmp = {0};
-    s21_from_float_to_decimal(0.5, &tmp);
+    s21_decimal tmp = {{5, 0, 0, -2147418112}};
     s21_add(value, tmp, result);
     s21_truncate(*result, result);
   }
@@ -61,19 +67,13 @@ int s21_floor(s21_decimal value, s21_decimal *result) {
     result_code = 1;
   } else {
     clear_decimal(result);
-
-    s21_decimal tmp = get_new_decimal();
-    s21_decimal tmp_sub = get_new_decimal();;
-
-    result_code = s21_truncate(value, &tmp);
-    if (result_code == 0) {
-      result_code = s21_sub(value, tmp, &tmp_sub);
-    }
-
-    for (int i = 0; i < 96 && result_code == 0; i++) {
-      if (get_decimal_digit_by_index(tmp_sub, i)) {
-        s21_sub(value, get_decimal_with_int_value(1), &value);
-        break;
+    if (get_decimal_exponent(value)) {
+      if (get_decimal_sign(value)) {
+        s21_truncate(value, &value);
+        s21_add(value, get_decimal_with_int_value(-1), &value);
+      }
+      else {
+        s21_truncate(value, &value);
       }
     }
     *result = value;
--- src/tests/s21_truncate_tests.c
+++ src/tests/s21_truncate_tests.c
@@ -3,85 +3,61 @@
 #include <check.h>
 
 START_TEST(test_truncate_0) {
-  s21_decimal decimal = get_new_decimal();
+  s21_decimal decimal = {{1302546422, -1587415236, 4052, -2146762752}};
+  s21_decimal test = {{254048838, 174, 0, -2147483648}};
   s21_decimal result = get_new_decimal();
-  s21_decimal test = get_new_decimal();
-  float x = 65634.937884;
-  s21_from_float_to_decimal(x, &decimal);
-  int ix = (int)x;
-  s21_from_int_to_decimal(ix, &test);
-
-  ck_assert_int_eq(0, s21_truncate(decimal, &result));
+  int test_res = s21_truncate(decimal, &result);
+  ck_assert_int_eq(0, test_res);
   ck_assert_int_eq(1, s21_is_equal(result, test));
 }
 END_TEST
 
 START_TEST(test_truncate_1) {
-  s21_decimal decimal = get_new_decimal();
+  s21_decimal decimal = {{1302546422, -1587415236, 4052, 720896}};
+  s21_decimal test = {{254048838, 174, 0, 0}};
   s21_decimal result = get_new_decimal();
-  s21_decimal test = get_new_decimal();
-  float x = -65634.937884;
-  s21_from_float_to_decimal(x, &decimal);
-  int ix = (int)x;
-  s21_from_int_to_decimal(ix, &test);
-
-  ck_assert_int_eq(0, s21_truncate(decimal, &result));
+  int test_res = s21_truncate(decimal, &result);
+  ck_assert_int_eq(0, test_res);
   ck_assert_int_eq(1, s21_is_equal(result, test));
 }
 END_TEST
 
 START_TEST(test_truncate_2) {
-  s21_decimal decimal = get_new_decimal();
+  s21_decimal decimal = {{2023177216, -2034508782, 40, 589824}};
+  s21_decimal test = {{254048838, 174, 0, 0}};
   s21_decimal result = get_new_decimal();
-  s21_decimal test = get_new_decimal();
-  float x = -65634.937884;
-  s21_from_float_to_decimal(x, &decimal);
-  int ix = (int)x;
-  s21_from_int_to_decimal(ix, &test);
-
-  ck_assert_int_eq(0, s21_truncate(decimal, &result));
+  int test_res = s21_truncate(decimal, &result);
+  ck_assert_int_eq(0, test_res);
   ck_assert_int_eq(1, s21_is_equal(result, test));
 }
 END_TEST
 
 START_TEST(test_truncate_3) {
-  s21_decimal decimal = get_new_decimal();
+  s21_decimal decimal = {{0, 0, 0, 589824}};
+  s21_decimal test = {{0, 0, 0, 0}};
   s21_decimal result = get_new_decimal();
-  s21_decimal test = get_new_decimal();
-  float x = -6563456.000;
-  s21_from_float_to_decimal(x, &decimal);
-  int ix = (int)x;
-  s21_from_int_to_decimal(ix, &test);
-
-  ck_assert_int_eq(0, s21_truncate(decimal, &result));
+  int test_res = s21_truncate(decimal, &result);
+  ck_assert_int_eq(0, test_res);
   ck_assert_int_eq(1, s21_is_equal(result, test));
 }
 END_TEST
 
 START_TEST(test_truncate_4) {
-  s21_decimal decimal = get_new_decimal();
+  s21_decimal decimal = {{-1530661241, 1528, 0, 0}};
+  s21_decimal test = {{-1530661241, 1528, 0, 0}};
   s21_decimal result = get_new_decimal();
-  s21_decimal test = get_new_decimal();
-  float x = 0.22113434;
-  s21_from_float_to_decimal(x, &decimal);
-  int ix = (int)x;
-  s21_from_int_to_decimal(ix, &test);
-
-  ck_assert_int_eq(0, s21_truncate(decimal, &result));
+  int test_res = s21_truncate(decimal, &result);
+  ck_assert_int_eq(0, test_res);
   ck_assert_int_eq(1, s21_is_equal(result, test));
 }
 END_TEST
 
 START_TEST(test_truncate_5) {
-  s21_decimal decimal = get_new_decimal();
+  s21_decimal decimal = {{-1530661241, 1528, 0, -2147483648}};
+  s21_decimal test = {{-1530661241, 1528, 0, -2147483648}};
   s21_decimal result = get_new_decimal();
-  s21_decimal test = get_new_decimal();
-  float x = 0.000000000;
-  s21_from_float_to_decimal(x, &decimal);
-  int ix = (int)x;
-  s21_from_int_to_decimal(ix, &test);
-
-  ck_assert_int_eq(0, s21_truncate(decimal, &result));
+  int test_res = s21_truncate(decimal, &result);
+  ck_assert_int_eq(0, test_res);
   ck_assert_int_eq(1, s21_is_equal(result, test));
 }
 END_TEST
