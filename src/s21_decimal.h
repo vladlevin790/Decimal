e685dfa092b16fe21f2a48ad67d4430ff793431e
David Anette <davidane@us-i4.yar.21-school.ru>
2024-07-24 19:05:14 +0300

    1

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -59,8 +59,6 @@ int s21_is_less(s21_decimal value_1, s21_decimal value_2);
 int s21_is_less_big(s21_big_decimal value_1, s21_big_decimal value_2,
                     int sign_1, int sign_2);
 int s21_is_less_or_equal(s21_decimal value_1, s21_decimal value_2);
-int s21_is_less_or_equal_big(s21_big_decimal value_1, s21_big_decimal value_2,
-                             int sign_1, int sign_2);
 
 // arithmetic
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result);
