b2d19db0e22fd4faab09e37031136b0e31624a75
PC <crunchydeer30@gmail.com>
2024-08-27 21:31:11 +0300

    decompose s21_from_decimal_to_float

--- src/s21_from_decimal_to_float/s21_from_decimal_to_float.c
+++ src/s21_from_decimal_to_float/s21_from_decimal_to_float.c
@@ -1,19 +1,11 @@
-#include <math.h>
-
-#include "decimal_helper/s21_decimal_helper.h"
-#include "s21_decimal.h"
+#include "s21_from_decimal_to_float.h"
 
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
-  //int result_code = S21_DECIMAL_OK;
   int result_code = CODE_CONVERTATION_ERROR;
 
   if (dst != NULL && !s21_check_decimal(src)) {
     result_code = S21_DECIMAL_OK;
     *dst = 0;
-  //if (dst == NULL || s21_check_decimal(src)) {
-  //  result_code = CODE_CONVERTATION_ERROR;
-  //} else {
-  //  *dst = 0;
 
     for (int i = 0; i < 96; i++) {
       *dst += s21_get_decimal_digit_by_index(src, i) * pow(2, i);
---
+++ src/s21_from_decimal_to_float/s21_from_decimal_to_float.h
@@ -0,0 +1,11 @@
+#ifndef S21_FROM_DECIMAL_TO_FLOAT_H
+#define S21_FROM_DECIMAL_TO_FLOAT_H
+
+#include <math.h>
+
+#include "../decimal_helper/s21_decimal_helper.h"
+#include "../s21_decimal.h"
+
+int s21_from_decimal_to_float(s21_decimal src, float *dst);
+
+#endif
