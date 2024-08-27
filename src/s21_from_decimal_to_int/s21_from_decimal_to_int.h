a4f67aab60c4d54f3d6af2e7715f590dfc0918b0
PC <crunchydeer30@gmail.com>
2024-08-27 21:35:40 +0300

    decompose s21_from_decimal_to_int

---
+++ src/s21_from_decimal_to_int/s21_from_decimal_to_int.h
@@ -0,0 +1,12 @@
+#ifndef S21_FROM_DECIMAL_TO_INT_H
+#define S21_FROM_DECIMAL_TO_INT_H
+
+#include <math.h>
+
+#include "../binary/s21_binary_operations.h"
+#include "../decimal_helper/s21_decimal_helper.h"
+#include "../s21_decimal.h"
+
+int s21_from_decimal_to_int(s21_decimal src, int *dst);
+
+#endif
